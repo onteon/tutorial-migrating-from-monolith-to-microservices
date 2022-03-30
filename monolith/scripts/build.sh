90691a284efdcf2b29fd82d277bce6f5753a7c68
Patryk Borchowiec <patryk.borchowiec@jlupin.com>
2022-03-30 14:06:29 +0200

    Added docker-compose file. Added onteon configuration and distribution. Added tomcat configuration. Updated urls.

--- monolith/scripts/build.sh
+++ monolith/scripts/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do
   DIR="$(cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd)"
@@ -13,6 +15,8 @@ DOWNLOAD_DIR="${ROOT_DIR}/download"
 TOMCAT_SOURCE="${DOWNLOAD_DIR}/tomcat.tar.gz"
 TOMCAT_DOWNLOAD_URL="http://archive.apache.org/dist/tomcat/tomcat-9/v9.0.60/bin/apache-tomcat-9.0.60.tar.gz"
 TOMCAT_DIR_NAME="apache-tomcat-9.0.60"
+APP_BIN_DIR="${TARGET_DIR}/app/bin"
+APP_CONF_DIR="${TARGET_DIR}/app/conf"
 
 
 mkdir -p "${DOWNLOAD_DIR}"
@@ -22,5 +26,11 @@ fi
 
 cd "${ROOT_DIR}" && mvn clean package
 tar -xvf "${TOMCAT_SOURCE}" -C "${TARGET_DIR}"
-rm -rf "${TARGET_DIR}/${TOMCAT_DIR_NAME}/webapps/"*
-cp -rf "${TARGET_DIR}/monolith.war" "${TARGET_DIR}/${TOMCAT_DIR_NAME}/webapps/"
+mkdir -p "${APP_BIN_DIR}"
+mkdir -p "${APP_CONF_DIR}"
+cp -r "${TARGET_DIR}/${TOMCAT_DIR_NAME}/"* "${APP_BIN_DIR}"
+rm -rf "${APP_BIN_DIR}/webapps/"*
+cp -r "${TARGET_DIR}/monolith.war" "${APP_BIN_DIR}/webapps/ROOT.war"
+cp "${ROOT_DIR}/onteon/conf.yml" "${APP_CONF_DIR}"
+cp -r "${ROOT_DIR}/tomcat/"* "${APP_BIN_DIR}"
+cd "${TARGET_DIR}/app" && tar -zcvf "${TARGET_DIR}/monolith.tar.gz" *
--- monolith/scripts/run_database.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -e POSTGRES_PASSWORD=password -p 5432:5432 postgres
